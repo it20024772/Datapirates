7b096675b17c9c46dcb9ba40da768b09e0d2ba0c
savi3307 <it20636074@my.sliit.lk>
2022-05-03 17:15:29 +0530

    GoalAdd java fix again

--- app/src/main/java/com/example/datapirates/goalAdd.java
+++ app/src/main/java/com/example/datapirates/goalAdd.java
@@ -94,3 +94,109 @@ public class goalAdd extends AppCompatActivity {
                 timePickerDialog.show();
             }
         });
+
+        savebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bookName = findViewById(R.id.edtBookName);
+                durationHours = findViewById(R.id.edtGoalHoursUpdate);
+                durationMinutes = findViewById(R.id.edtGoalMinutesUpdate);
+                reminderBox = findViewById(R.id.reminderCb);
+
+                // set notification for reminder
+                if (reminderBox.isChecked()) {
+                    Intent intent = new Intent(goalAdd.this, reminderBroadcast.class);
+                    PendingIntent pendingIntent = PendingIntent.getBroadcast(goalAdd.this, 0, intent, 0);
+                    AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+                    long nowtime = System.currentTimeMillis();
+                    long add = 1000 * 10;
+                    alarmManager.setExact(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(), pendingIntent);
+                    Log.i("timecal", String.valueOf(c.getTimeInMillis()));
+                }
+
+                String bookName_txt = bookName.getText().toString();
+                String durationHours_txt = durationHours.getText().toString();
+                String durationMinutes_txt = durationMinutes.getText().toString();
+
+                // validations
+                if (TextUtils.isEmpty(bookName_txt)) {
+                    Toast.makeText(goalAdd.this, "Please enter book name", Toast.LENGTH_SHORT).show();
+                } else if (TextUtils.isEmpty(durationHours_txt)) {
+                    Toast.makeText(goalAdd.this, "Please enter duration hours", Toast.LENGTH_SHORT).show();
+                } else if (TextUtils.isEmpty(durationMinutes_txt)) {
+                    Toast.makeText(goalAdd.this, "Please enter duration minutes", Toast.LENGTH_SHORT).show();
+                } else if (c == null) {
+                    Toast.makeText(goalAdd.this, "Please set start time", Toast.LENGTH_SHORT).show();
+                } else {
+                    saveData(bookName_txt, durationHours_txt, durationMinutes_txt, hour, minute);
+                }
+
+
+            }
+        });
+
+        // back button functionality
+        backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // got to dashboard
+                Intent Dashboard = new Intent(goalAdd.this, Dashboard.class);
+                startActivity(Dashboard);
+                finish();
+            }
+        });
+
+
+    }
+
+    private void saveData(String bookName_txt, String durationHours_txt, String durationMinutes_txt, int hour, int minute) {
+
+        String time = String.format(Locale.getDefault(), "%02d:%02d",hour,minute);
+
+        // add details to the goal object
+        goal.setName(bookName_txt);
+        goal.setDurationHours(durationHours_txt);
+        goal.setDurationMinutes(durationMinutes_txt);
+        goal.setStartTime(time);
+
+        if (reminderBox.isChecked()){
+            goal.setRemind(true);
+        }
+        else {
+            goal.setRemind(false);
+        }
+
+        // add to database
+        databaseReference.setValue(goal).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(goalAdd.this, "Goal Created", Toast.LENGTH_SHORT).show();
+                    // got to goal display page
+                    Intent goalShow = new Intent(goalAdd.this,goalShow.class);
+                    startActivity(goalShow);
+                    finish();
+                }
+                else{
+                    Toast.makeText(goalAdd.this, "An error occurred. Please try again later.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "BookwormReminderChannel";
+            String description = "Channel to remind reading goal time";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("notifyBookworm", name, importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+}
+
