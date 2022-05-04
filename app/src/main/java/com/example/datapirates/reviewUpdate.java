85378e90e6fd2839886ef61ae1e1e0ad94d21ec0
it20640484 <it20640484@my.sliit.lk>
2022-05-04 18:20:28 +0530

    reviewUpdate file added

---
+++ app/src/main/java/com/example/datapirates/reviewUpdate.java
@@ -0,0 +1,38 @@
+package com.example.datapirates;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
+import com.example.datapirates.model.Review;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class reviewUpdate extends AppCompatActivity {
+
+    private EditText bookName;
+    private TextInputEditText description;
+    private RatingBar ratingBar;
+    private FloatingActionButton savebtn;
+    private ImageView backArrow;
+
+    private FirebaseAuth auth;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+    private Review review;
+    private String userId;
+
