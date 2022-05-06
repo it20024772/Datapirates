c0203f85f13395fe8a4057a92aa9f3e069c201df
Heshani0121 <79798716+Heshani0121@users.noreply.github.com>
2022-05-06 10:25:25 +0530

    add progressshow

---
+++ app/src/main/java/com/example/datapirates/progressShow.java
@@ -0,0 +1,48 @@
+package com.example.datapirates;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.datapirates.model.Progress;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Calendar;
+import java.util.Locale;
+
+public class progressShow extends AppCompatActivity {
+    private EditText bookName, totalPages, currentPage;
+    private FloatingActionButton savebtn;
+    private ProgressBar progressBar;
+    private TextView progressPercentTxt;
+    private ImageView backArrow, addProgress, deleteProgress, editBookName, editTotalPages, editCurrentPage;
+    private ProgressDialog dialog;
+
+    private FirebaseAuth auth;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+    private String userId;
+    Progress progress;
